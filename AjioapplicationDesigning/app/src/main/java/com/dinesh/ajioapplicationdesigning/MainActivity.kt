77455d3e57bdf595fa7a34d10ce9481fc93f727c
rdinesh19 <42532648+rdinesh19@users.noreply.github.com>
2020-07-31 17:57:22 +0530

    Alter the previous activity_main screen look like Ajio Application screen.

--- AjioapplicationDesigning/app/src/main/java/com/dinesh/ajioapplicationdesigning/MainActivity.kt
+++ AjioapplicationDesigning/app/src/main/java/com/dinesh/ajioapplicationdesigning/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.dinesh.ajioapplicationdesigning
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
